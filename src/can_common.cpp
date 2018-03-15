c3ba92bf408eee56d5ac15f0619f0d5c0c0c7c15
BlueChip <csbluechip@gmail.com>
2018-03-15 00:52:50 +0000

    :csbluechip: Major code cleanup ; Tried to compile before uploading but (quel suprise) the Arduino interface doesn't play nicely with windows MKLINK links {sigh} ...it's 1am I have no idea if this compiles

--- src/can_common.cpp
+++ src/can_common.cpp
@@ -1,228 +1,258 @@
 #include <can_common.h>
 
-CANListener::CANListener()
+// !!! FFS. Is it a "handler", a "filter", or a "callback" ...pick a naming convention and stick with it!
+
+//+=====================================================================================================================
+CANListener::CANListener ()
 {
-	callbacksActive = 0; //none. Bitfield were bits 0-7 are the mailboxes and bit 8 is the general callback
     numFilters = 8;
+	initialize();
 }
 
-//an empty version so that the linker doesn't complain that no implementation exists.
-void CANListener::gotFrame(CAN_FRAME */*frame*/, int /*mailbox*/)
+//+=====================================================================================================================
+void  CANListener::initialize ()
 {
-  
+	callbacksActive = 0x00;  // Bits 0..7 are the mailboxe callbacks;  bit 8 is the general callback
 }
 
-void CANListener::attachMBHandler(uint8_t mailBox)
+//+=====================================================================================================================
+// !!! This needs to return a bool
+void  CANListener::attachMBHandler (uint8_t mailbox)
 {
-	if ( mailBox < numFilters )
-	{
-		callbacksActive |= (1<<mailBox);
-	}
+	if (mailBox >= numFilters)  return ;//false;
+
+	callbacksActive |= (1 << mailbox) ;
+	return ;//true;
 }
 
-void CANListener::detachMBHandler(uint8_t mailBox)
+//+=====================================================================================================================
+// !!! This needs to return a bool
+void  CANListener::detachMBHandler (uint8_t mailbox)
 {
-	if ( mailBox < numFilters )
-	{
-		callbacksActive &= ~(1<<mailBox);
-	}  
+	if (mailBox >= numFilters)  return ;//false;
+
+	callbacksActive &= ~(1 << mailbox) ;
+	return ;//true;
 }
 
-void CANListener::attachGeneralHandler()
+//+=====================================================================================================================
+void  CANListener::attachGeneralHandler ()
 {
 	callbacksActive |= (1 << numFilters);
 }
 
-void CANListener::detachGeneralHandler()
+//+=====================================================================================================================
+void  CANListener::detachGeneralHandler ()
 {
 	callbacksActive &= ~(1 << numFilters);
 }
 
-void CANListener::initialize()
+//+=====================================================================================================================
+bool  CANListener::isCallbackActive (int callback)
 {
-   callbacksActive = 0;
+    return (callbacksActive & (1 << callback)) ? true : false ;
 }
 
-bool CANListener::isCallbackActive(int callback)
+//+=====================================================================================================================
+// An empty version so that the linker doesn't complain that no implementation exists.
+//
+void  CANListener::gotFrame (CAN_FRAME* frame,  int mailbox)
 {
-    return (callbacksActive & (1 << callback))?true:false;
+	(void)frame;
+	(void)mailbox;
 }
 
+//**********************************************************************************************************************
+//**********************************************************************************************************************
 
-
-/*
-Some functions in CAN_COMMON are declared = 0 which means you HAVE to reimplement them or the compiler will puke. Some functions are optional
-and they've got blank or very simple implementations below.
-*/
-
-CAN_COMMON::CAN_COMMON(int numFilt)
+//+=====================================================================================================================
+// Some functions in CAN_COMMON are declared = 0 which means you HAVE to reimplement them or the compiler will puke.
+// Some functions are optional and they've got blank or very simple implementations below.
+//
+CAN_COMMON::CAN_COMMON (int numFilt)
 {
     numFilters = numFilt;
-    //Official entry for the worst, most convoluted looking C++ line ever written.
-    //Dynamically allocate enough space for the function pointers with a hideous malloc call.
+
+	// !!! what's going on with this missing malloc() ??
+	// !!! oh, I see, we declare a fixed size array of 16 callbacks
+	// !!! typdef the function pointer and do this properly/readably
+    // Official entry for the worst, most convoluted looking C++ line ever written.
+    // Dynamically allocate enough space for the function pointers with a hideous malloc call.
     //cbCANFrame = malloc(4 * numFilters);
-    memset(cbCANFrame, 0, 4 * numFilters);
+    memset(cbCANFrame, 0, sizeof(void*) * numFilters);
+
     cbGeneral = 0;
     enablePin = 255;
-    for (int i = 0; i < SIZE_LISTENERS; i++) listener[i] = 0;
+    for (int  i = 0;  i < SIZE_LISTENERS;  listener[i++] = 0) ;
 }
 
-uint32_t CAN_COMMON::begin()
+//+=====================================================================================================================
+uint32_t  CAN_COMMON::begin ()
 {
 	return init(CAN_DEFAULT_BAUD);
 }
 
-uint32_t CAN_COMMON::begin(uint32_t baudrate)
+//+=============================================================================
+uint32_t  CAN_COMMON::begin (uint32_t baud)
 {
-	return init(baudrate);
+	return init(baud);
 }
 
-uint32_t CAN_COMMON::begin(uint32_t baudrate, uint8_t enPin) 
+//+=============================================================================
+uint32_t  CAN_COMMON::begin (uint32_t baud,  uint8_t enPin)
 {
 	enablePin = enPin;
-    return init(baudrate);
+    return init(baud);
 }
 
-uint32_t CAN_COMMON::getBusSpeed()
+//+=====================================================================================================================
+uint32_t  CAN_COMMON::getBusSpeed ()
 {
 	return busSpeed;
 }
 
-boolean CAN_COMMON::attachObj(CANListener *listener)
+//+=====================================================================================================================
+bool  CAN_COMMON::attachObj (CANListener* listener)
 {
-	for (int i = 0; i < SIZE_LISTENERS; i++)
-	{
-		if (this->listener[i] == NULL)
-		{
+	// Use the first available listener
+	for (int  i = 0;  i < SIZE_LISTENERS;  i++)
+		if (this->listener[i] == NULL) {
 			this->listener[i] = listener;
-			listener->initialize();
-			return true;			
+			listener->initialize();  // !!! get initialize to return its own pointer so we can move this up a line
+			return true;
 		}
-	}
 	return false;
 }
 
-boolean CAN_COMMON::detachObj(CANListener *listener)
+//+=====================================================================================================================
+bool  CAN_COMMON::detachObj (CANListener* listener)
 {
-	for (int i = 0; i < SIZE_LISTENERS; i++)
-	{
-		if (this->listener[i] == listener)
-		{
-			this->listener[i] = NULL;			
-			return true;			
+	for (int  i = 0;  i < SIZE_LISTENERS;  i++)
+		if (this->listener[i] == listener) {
+			this->listener[i] = NULL;
+			return true;
 		}
-	}
-	return false;  
+	return false;
 }
 
-/**
- * \brief Set up a general callback that will be used if no callback was registered for receiving mailbox
- *
- * \param cb A function pointer to a function with prototype "void functionname(CAN_FRAME *frame);"
- *
- * \note If this function is used to set up a callback then no buffering of frames will ever take place.
- */
-void CAN_COMMON::setGeneralCallback(void (*cb)(CAN_FRAME *))
+//+=====================================================================================================================
+// Set default callback for mailboxes with no registered callback
+// If this function is used to set up a callback then no buffering of frames will ever take place.
+//
+void  CAN_COMMON::setGeneralCallback (void (*cb)(CAN_FRAME*))
 {
 	cbGeneral = cb;
 }
 
-/**
- * \brief Set up a callback function for given mailbox
- *
- * \param mailbox Which mailbox (0-7) to assign callback to.
- * \param cb A function pointer to a function with prototype "void functionname(CAN_FRAME *frame);"
- *
- */
-void CAN_COMMON::setCallback(uint8_t mailbox, void (*cb)(CAN_FRAME *))
+//+=====================================================================================================================
+// Set up a callback for a specific mailbox
+//
+// !!! needs to return bool
+void  CAN_COMMON::setCallback (uint8_t mailbox,  void (*cb)(CAN_FRAME*))
 {
-	if ( mailbox >= numFilters ) return;
+	if (mailbox >= numFilters)  return ;//false;
 	cbCANFrame[mailbox] = cb;
+	return ;//true;
 }
 
-void CAN_COMMON::attachCANInterrupt(uint8_t mailBox, void (*cb)(CAN_FRAME *)) 
+//+=============================================================================
+// !!! wrapper for setCallback !?
+void  CAN_COMMON::attachCANInterrupt (uint8_t mailBox,  void (*cb)(CAN_FRAME*))
 {
 	setCallback(mailBox, cb);
 }
 
-void CAN_COMMON::detachCANInterrupt(uint8_t mailBox)
+//+=====================================================================================================================
+// !!! needs to return bool
+void  CAN_COMMON::detachCANInterrupt (uint8_t mailBox)
 {
-	if ( mailBox >= numFilters ) return;
+	if (mailbox >= numFilters)  return ;//false;
 	cbCANFrame[mailBox] = 0;
+	return ;//true;
 }
 
-int CAN_COMMON::setRXFilter(uint8_t mailbox, uint32_t id, uint32_t mask, bool extended)
-{
-    return _setFilterSpecific(mailbox, id, mask, extended);
-}
-
-int CAN_COMMON::setRXFilter(uint32_t id, uint32_t mask, bool extended)
+//+=====================================================================================================================
+int  CAN_COMMON::setRXFilter (uint32_t id,  uint32_t mask,  bool extended)
 {
     return _setFilter(id, mask, extended);
 }
 
-int CAN_COMMON::watchFor() 
+//+============================================================================
+int  CAN_COMMON::setRXFilter (uint8_t mailbox,  uint32_t id,  uint32_t mask,  bool extended)
 {
-    setRXFilter(0, 0, false);
-	return setRXFilter(0, 0, true);
-
+    return _setFilterSpecific(mailbox, id, mask, extended);
 }
 
-//Let a single frame ID through. Automatic determination of extended. Also automatically sets mask
-int CAN_COMMON::watchFor(uint32_t id)
+//+=====================================================================================================================
+int  CAN_COMMON::watchFor ()
 {
-	if (id > 0x7FF) return setRXFilter(id, 0x1FFFFFFF, true);
-	else return setRXFilter(id, 0x7FF, false);
+	if (id <= 0x7FF)  return setRXFilter(0, 0, false) ;
+	else              return setRXFilter(0, 0, true ) ;
 }
 
-//Allow a range of IDs through based on mask. Still auto determines extended.
-int CAN_COMMON::watchFor(uint32_t id, uint32_t mask)
+//+=============================================================================
+// Let a single frame ID through.
+// Automatic determination of extended & mask
+//
+int  CAN_COMMON::watchFor (uint32_t id)
 {
-	if (id > 0x7FF) return setRXFilter(id, mask, true);
-	else return setRXFilter(id, mask, false);
+	if (id <= 0x7FF)  return setRXFilter(id,      0x7FF, false) ;
+	else              return setRXFilter(id, 0x1FFFFFFF, true ) ;
 }
 
-//A bit more complicated. Makes sure that the range from id1 to id2 is let through. This might open
-//the floodgates if you aren't careful.
-//There are undoubtedly better ways to calculate the proper values for the filter but this way seems to work.
-//It'll be kind of slow if you try to let a huge span through though.
-int CAN_COMMON::watchForRange(uint32_t id1, uint32_t id2)
+//+=============================================================================
+// Allow a range of IDs through based on mask.
+// Automatic determination of extended (only)
+//
+int  CAN_COMMON::watchFor (uint32_t id,  uint32_t mask)
 {
-	uint32_t id = 0;
-	uint32_t mask = 0;
-	uint32_t temp;
+	if (id <= 0x7FF)  return setRXFilter(id, mask, false) ;
+	else              return setRXFilter(id, mask, true ) ;
+}
 
-	if (id1 > id2) 
-	{   //looks funny I know. In place swap with no temporary storage. Neato!
-		id1 = id1 ^ id2;
-		id2 = id1 ^ id2; //note difference here.
-		id1 = id1 ^ id2;
+//+=============================================================================
+// Makes sure that the range from id1 to id2 is let through.
+// This might open the floodgates if you aren't careful.
+// There are undoubtedly better ways to do this, but this way seems to work.
+// It'll be kind of slow if you try to let a huge span through though.
+//
+// Here is a quick overview of the theory behind these calculations:
+//   We start with mask set to 11 or 29 set bits (all 1's)
+//   and id set to the lowest ID in the range.
+//
+//   We then loop through every single ID possible in the range.
+//   For each ID we AND with the current ID.
+//   At the end only bits that never changed and were 1's will still be 1's.
+//   This yields the ID we can match against to let these frames through.
+//
+//   The mask is calculated by finding the bitfield difference between the lowest ID and the current ID.
+//   This calculation will be 1 anywhere the bits were different.
+//   We invert this so that it is 1 anywhere the bits where the same.
+//   Then we AND with the current Mask.
+//
+//   At the end the mask will be 1 anywhere the bits never changed.
+//   ...This is the perfect mask.
+//
+//
+int  CAN_COMMON::watchForRange (uint32_t idLo,  uint32_t idHi)
+{
+	uint32_t  id;
+	uint32_t  mask;
+
+	// Ensure id1 < id2
+	if (idLo > idHi) {
+		uint32_t  tmp = idLo;
+		idLo = idHi;
+		idHi = tmp;
 	}
 
-	id = id1;
-
-	if (id2 <= 0x7FF) mask = 0x7FF;
-	else mask = 0x1FFFFFFF;
-
-	/* Here is a quick overview of the theory behind these calculations.
-	   We start with mask set to 11 or 29 set bits (all 1's)
-	   and id set to the lowest ID in the range.
-	   From there we go through every single ID possible in the range. For each ID
-	   we AND with the current ID. At the end only bits that never changed and were 1's
-	   will still be 1's. This yields the ID we can match against to let these frames through
-	   The mask is calculated by finding the bitfield difference between the lowest ID and
-	   the current ID. This calculation will be 1 anywhere the bits were different. We invert
-	   this so that it is 1 anywhere the bits where the same. Then we AND with the current Mask.
-	   At the end the mask will be 1 anywhere the bits never changed. This is the perfect mask.
-	*/
-	for (uint32_t c = id1; c <= id2; c++)
-	{
-		id &= c;
-		temp = (~(id1 ^ c)) & 0x1FFFFFFF;
-		mask &= temp;
+	id   = id1;
+	mask = (id2 <= 0x7FF) ? 0x7FF : 0x1FFFFFFF ;
+	for (uint32_t  c = id1;  c <= id2;  c++) {
+		id   &= c;
+		mask &= (~(id1 ^ c)) & 0x1FFFFFFF;
 	}
-	//output of the above crazy loop is actually the end result.
-	if (id > 0x7FF) return setRXFilter(id, mask, true);
-	else return setRXFilter(id, mask, false);
-}
 
+	// output of the above crazy loop is actually the end result.
+	return watchFor(id, mask);
+}
--- src/can_common.h
+++ src/can_common.h
@@ -3,18 +3,20 @@
 
 #include <Arduino.h>
 
-/** Define the typical baudrate for CAN communication. */
-#ifdef CAN_BPS_500K
-#undef CAN_BPS_1000K 
-#undef CAN_BPS_800K                 
-#undef CAN_BPS_500K                 
-#undef CAN_BPS_250K                 
-#undef CAN_BPS_125K                  
-#undef CAN_BPS_50K                   
-#undef CAN_BPS_33333				  
-#undef CAN_BPS_25K                   
-#undef CAN_BPS_10K                   
-#undef CAN_BPS_5K                   
+//----------------------------------------------------------------------------------------------------------------------
+// #defines for standard baud rates
+//
+#ifdef CAN_BPS_500K  // !!! why are we doing this?? - what else defines them?
+#	undef CAN_BPS_1000K
+#	undef CAN_BPS_800K
+#	undef CAN_BPS_500K
+#	undef CAN_BPS_250K
+#	undef CAN_BPS_125K
+#	undef CAN_BPS_50K
+#	undef CAN_BPS_33333
+#	undef CAN_BPS_25K
+#	undef CAN_BPS_10K
+#	undef CAN_BPS_5K
 #endif
 
 #define CAN_BPS_1000K	1000000
@@ -28,112 +30,164 @@
 #define CAN_BPS_10K		10000
 #define CAN_BPS_5K		5000
 
+#define CAN_DEFAULT_BAUD  CAN_BPS_250K  // !!! CAN_BPS_DFLT
+
 #define SIZE_LISTENERS	4 //number of classes that can register as listeners with this class
-#define CAN_DEFAULT_BAUD	CAN_BPS_250K
 
-//This structure presupposes little endian mode. If you use it on a big endian processor you're going to have a bad time.
-typedef union {
-  uint64_t value;
+//----------------------------------------------------------------------------------------------------------------------
+// This structure presupposes little endian mode.
+// If you use it on a big endian processor you're going to have a bad time.
+//
+// !!! this simple doesn't work 0x0102030405060708 -> {8,7,6,5,4,3,2,1}
+// !!! This whole idea needs to be revisited - probably with MACROs
+typedef
+	union {
+		uint64_t value;
+		struct {
+			uint32_t low;
+			uint32_t high;
+		};
+		struct {
+			uint16_t s0;
+			uint16_t s1;
+			uint16_t s2;
+			uint16_t s3;
+		};
+		uint8_t byte[8];
+		uint8_t bytes[8]; // !!! lose it
+	}
+BytesUnion;  // !!! bytesUnion_t
+
+//----------------------------------------------------------------------------------------------------------------------
+/*
+// !!! I propose this replacement
+
+typedef
+  enum canDir {
+    CDIR_RX = 0,
+    CDIR_TX = 1,
+  }
+canDir_t;
+
+typedef
+  struct canFrame {
+    // Defined in a strange order to help the compiler optimise data storage
+    uint32_t  mS;       // mS - the CAN timer is too small, save the due timestamp as well!
+    uint32_t  aid;      // Arbitration ID
+    uint32_t  fid;      // Family ID
+    uint8_t   data[8];  // Payload
+    uint16_t  ts;       // Timestamp
+    uint8_t   dlc;      // Length {0..8}
+    uint8_t   pri;      // Priority {0..15} - Only used when transmitting frames
+    bool      ext;      // Extended
+    bool      rtr;      // Remote Tranmit Request
+    uint8_t   bus;      // CAN Bus number   - Used by data logging systems
+    canDir_t  dir;      // CDIR_RX, CDIR_TX - Used by data logging systems
+  }
+canFrame_t;
+
+*/
+typedef
 	struct {
-		uint32_t low;
-		uint32_t high;
-	};
-	struct {
-		uint16_t s0;
-		uint16_t s1;
-		uint16_t s2;
-		uint16_t s3;
-  };
-	uint8_t bytes[8];
-	uint8_t byte[8]; //alternate name so you can omit the s if you feel it makes more sense
-} BytesUnion;
+		uint32_t    id;        // EID if ide set, SID otherwise
+		uint32_t    fid;       // family ID
+		uint8_t     rtr;       // Remote Transmission Request
+		uint8_t     priority;  // Priority but only important for TX frames and then only for special uses.
+		uint8_t     extended;  // Extended ID flag
+		uint16_t    time;      // CAN timer value when mailbox message was received.
+		uint8_t     length;    // Number of data bytes
+		BytesUnion  data;      // 64 bits - lots of ways to access it.
+	}
+CAN_FRAME; // !!! canFrame_t
 
-typedef struct
-{
-  uint32_t id;		// EID if ide set, SID otherwise
-  uint32_t fid;		// family ID
-  uint8_t rtr;		// Remote Transmission Request
-  uint8_t priority;	// Priority but only important for TX frames and then only for special uses.
-  uint8_t extended;	// Extended ID flag
-  uint16_t time;      // CAN timer value when mailbox message was received.
-  uint8_t length;		// Number of data bytes
-  BytesUnion data;	// 64 bits - lots of ways to access it.
-} CAN_FRAME;
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class CANListener
 {
 public:
-  CANListener();
+	CANListener                () ;  // Constructor
 
-  virtual void gotFrame(CAN_FRAME *frame, int mailbox);
+	void  initialize           () ;
+	void  setNumFilters        (int numFilt) ;
 
-  void attachMBHandler(uint8_t mailBox);
-  void detachMBHandler(uint8_t mailBox);
-  void attachGeneralHandler();
-  void detachGeneralHandler();
-  void initialize();
-  bool isCallbackActive(int callback);
-  void setNumFilters(int numFilt);
+	void  attachMBHandler      (uint8_t mailBox) ;
+	void  detachMBHandler      (uint8_t mailBox) ;
+
+	void  attachGeneralHandler () ;
+	void  detachGeneralHandler () ;
+
+	bool  isCallbackActive     (int callback) ;
+
+	virtual void  gotFrame     (CAN_FRAME* frame, int mailbox) ;
 
 private:
-  int callbacksActive; //bitfield letting the code know which callbacks to actually try to use (for object oriented callbacks only)
-  int numFilters; //filters, mailboxes, whichever, how many do we have?
-
+	int  callbacksActive; // Bitfield of active callbacks - Only for object-oriented callbacks
+	int  numFilters;      // filters, mailboxes, whichever, how many do we have? !!! yeah, pick a naming convention
 };
 
-/*Abstract function that mostly just sets an interface that all descendants must implement */
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Abstract function that mostly just sets an interface that all descendants must implement
 class CAN_COMMON
 {
 public:
+	CAN_COMMON (int numFilt) ;  // Constructor
 
-    CAN_COMMON(int numFilt);
+	//Public API that needs to be re-implemented by subclasses
+	virtual  uint32_t  init               (uint32_t ul_baudrate) = 0 ;
 
-    //Public API that needs to be re-implemented by subclasses
-	virtual int _setFilterSpecific(uint8_t mailbox, uint32_t id, uint32_t mask, bool extended) = 0;
-    virtual int _setFilter(uint32_t id, uint32_t mask, bool extended) = 0;
-	virtual uint32_t init(uint32_t ul_baudrate) = 0;
-    virtual uint32_t beginAutoSpeed() = 0;
-    virtual uint32_t set_baudrate(uint32_t ul_baudrate) = 0;
-    virtual void setListenOnlyMode(bool state) = 0;
-	virtual void enable() = 0;
-	virtual void disable() = 0;
-	virtual bool sendFrame(CAN_FRAME& txFrame) = 0;
-	virtual bool rx_avail() = 0;
-	virtual uint16_t available() = 0; //like rx_avail but returns the number of waiting frames
-	virtual uint32_t get_rx_buff(CAN_FRAME &msg) = 0;
+	virtual  int       _setFilter         (/*-----------*/   uint32_t id,  uint32_t mask,  bool extended) = 0 ;
+	virtual  int       _setFilterSpecific (uint8_t mailbox,  uint32_t id,  uint32_t mask,  bool extended) = 0 ;
 
-    //Public API common to all subclasses - don't need to be re-implemented
-    //wrapper for syntactic sugar reasons
-    //note to my dumb self - functions cannot be both virtual and have multiple versions where the parameter list is different
-    //you have to pick one or the other.
-	inline uint32_t read(CAN_FRAME &msg) { return get_rx_buff(msg); }
-    int watchFor(); //allow anything through
-	int watchFor(uint32_t id); //allow just this ID through (automatic determination of extended status)
-    int watchFor(uint32_t id, uint32_t mask); //allow a range of ids through
-    int watchFor(uint32_t id, uint32_t mask, bool ext);
-	int watchForRange(uint32_t id1, uint32_t id2); //try to allow the range from id1 to id2 - automatically determine base ID and mask
-	uint32_t begin();
-	uint32_t begin(uint32_t baudrate);
-    uint32_t begin(uint32_t baudrate, uint8_t enPin);
-	uint32_t getBusSpeed();
-	int setRXFilter(uint8_t mailbox, uint32_t id, uint32_t mask, bool extended);
-    int setRXFilter(uint32_t id, uint32_t mask, bool extended);
-    boolean attachObj(CANListener *listener);
-	boolean detachObj(CANListener *listener);
-    void setGeneralCallback( void (*cb)(CAN_FRAME *) );
-    void attachCANInterrupt( void (*cb)(CAN_FRAME *) ) {setGeneralCallback(cb);}
-	void setCallback(uint8_t mailbox, void (*cb)(CAN_FRAME *));
-	void attachCANInterrupt(uint8_t mailBox, void (*cb)(CAN_FRAME *));
-	void detachCANInterrupt(uint8_t mailBox);
+	virtual  uint32_t  beginAutoSpeed     ()                     = 0 ;
+	virtual  uint32_t  set_baudrate       (uint32_t ul_baudrate) = 0 ;
+
+	virtual  void      setListenOnlyMode  (bool state)           = 0 ;
+
+	virtual  void      enable             ()                     = 0 ;
+	virtual  void      disable            ()                     = 0 ;
+
+	virtual  bool      sendFrame          (CAN_FRAME& txFrame)   = 0 ;
+
+	virtual  bool      rx_avail           ()                     = 0 ;
+	virtual  uint16_t  available          ()                     = 0 ; // Like rx_avail, but returns count of waiting frames
+	virtual  uint32_t  get_rx_buff        (CAN_FRAME& msg)       = 0 ;
+
+	// Public API common to all subclasses - don't need to be re-implemented
+	// wrapper for syntactic sugar reasons
+	// NB. Functions CANNOT be BOTH virtual AND overloaded
+	inline  uint32_t  read (CAN_FRAME &msg)  { return get_rx_buff(msg); }
+	int       watchFor      () ;                                        // Promiscuous mode
+	int       watchFor      (uint32_t id) ;                             // Specific AID (auto-std|ext)
+	int       watchFor      (uint32_t id,  uint32_t mask) ;             // AID_Mask (auto-std|ext)
+	int       watchFor      (uint32_t id,  uint32_t mask,  bool ext) ;  // AID_Mask (specify std|ext)
+	int       watchForRange (uint32_t idLo,  uint32_t idHi);            // Auto-AID_Mask and auto-std|ext
+
+	uint32_t  begin () ;
+	uint32_t  begin (uint32_t baudrate) ;
+	uint32_t  begin (uint32_t baudrate,  uint8_t enPin) ;
+
+	uint32_t  getBusSpeed () ;
+
+	int       setRXFilter (/*-----------*/   uint32_t id,  uint32_t mask,  bool extended);
+	int       setRXFilter (uint8_t mailbox,  uint32_t id,  uint32_t mask,  bool extended);
+
+	bool      attachObj (CANListener* listener);
+	bool      detachObj (CANListener* listener);
+
+	void      setGeneralCallback (void (*cb)(CAN_FRAME*));  // !!! typdef these function pointers!
+	void      attachCANInterrupt (void (*cb)(CAN_FRAME*))  { setGeneralCallback(cb); }  // !!! WHY, Just WHY?
+
+	void      setCallback        (uint8_t mailbox,  void (*cb)(CAN_FRAME*));
+	void      attachCANInterrupt (uint8_t mailBox,  void (*cb)(CAN_FRAME*));
+	void      detachCANInterrupt (uint8_t mailBox);
 
 protected:
-	CANListener *listener[SIZE_LISTENERS];
-    void (*cbGeneral)(CAN_FRAME *); //general callback if no per-mailbox or per-filter entries matched
-    void (*cbCANFrame[16])(CAN_FRAME *); //array of function pointers - disgusting syntax though.
-    uint32_t busSpeed;
-    int numFilters;
-    int enablePin;
+	void (*cbGeneral)     (CAN_FRAME*); // General callback if no per-mailbox or per-filter entries matched
+	void (*cbCANFrame[16])(CAN_FRAME*); // Array of function pointers - disgusting syntax though. !!! then typedef it!
+
+	CANListener*  listener[SIZE_LISTENERS];
+	uint32_t      busSpeed;
+	int           numFilters;
+	int           enablePin;
 };
 
 #endif
-
